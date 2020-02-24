429a79695d98880671ea88a0064dc59fa4651eaf
Rasha ali <rashaali84@gmail.com>
2020-02-24 19:20:44 +0100

    handlers update

--- handlers/reset.js
+++ handlers/reset.js
@@ -1,9 +1,14 @@
 function resetHandler() {
   debugger;
   // reset state to initial values
-
+  numbers.current = 0;
+  numbers.biggest = -Infinity;
+  numbers.smallest = Infinity;
+  numbers.all = [];
   // re-render UI with values saved in state
-
+  document.getElementById("input").value = "";
+  document.getElementById("biggest").innerText = Infinity;
+  document.getElementById("smallest").innerText = -Infinity;
   // log interaction: handler name, new state
   log.push({
     handler: 'reset',
--- handlers/save-number.js
+++ handlers/save-number.js
@@ -1,19 +1,22 @@
 function saveNumberHandler() {
   debugger;
   // read new number from user input
-
+  numbers.current = document.getElementById("input").value;
+  numbers.all.push(numbers.current);
   // read from state the data you will need for the next step
-
   // find the new biggest and smallest values
-
+  const smallest_number = Math.min(...numbers.all);
+  const largest_number = Math.max(...numbers.all);
   // update state: new biggest, new smallest, new current & save the last current
-
+  numbers.biggest = largest_number;
+  numbers.smallest = smallest_number;
   // re-render the user interface with values stored in state
-
+  document.getElementById("biggest").innerText = largest_number;
+  document.getElementById("smallest").innerText = smallest_number;
   // log user interaction: handler name, user input, new state
   log.push({
     handler: 'save number',
-    newInput,
+    newInput: numbers.current,
     numbers: JSON.parse(JSON.stringify(numbers))
   })
 }
